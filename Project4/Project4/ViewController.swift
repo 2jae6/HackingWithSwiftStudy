3e33bdeb0d7a1081dd8be7af44e1a33157c2692d
JaeWook Lee <2jae66@gmail.com>
2020-05-07 17:28:23 +0900

    project4

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -9,30 +9,82 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
-
-    var webView: WKWebView!
-    override func loadView() {
+class ViewController: UIViewController, WKNavigationDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return websites.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(websites[indexPath.row])"
+        return cell
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
-        let url = URL(string: "https://lazyowl.tistory.com")!
+        let url = URL(string: "https://" + websites[indexPath.row])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
-
-
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var progressView: UIProgressView!
+    var webView: WKWebView!
+    var websites = ["apple.com", "lazyowl.tistory.com", "hackingwithswift.com"]
+    
+    /*override func loadView() {
+     
+     webView = WKWebView()
+     webView.navigationDelegate = self
+     view = webView
+     
+     }
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        //UIToolbar
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        //goback, goforward
+        let goBack = UIBarButtonItem(barButtonSystemItem: .rewind, target: webView, action: #selector(webView.goBack))
+        let goForward = UIBarButtonItem(barButtonSystemItem: .add, target: webView, action: #selector(webView.goForward))
+        toolbarItems = [progressButton, spacer, goBack, spacer, goForward, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        //초기 테이블 뷰로 변경
+        /*
+         let url = URL(string: "https://" + websites[0])!
+         webView.load(URLRequest(url: url))
+         webView.allowsBackForwardNavigationGestures = true
+         */
+        
+        //KVO !!!
+        
+        //webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+    }
+    
+    
+    
     @objc func openTapped(){
         let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "lazyowl.tistory.com", style: .default, handler: openPage))
+        for website in websites{
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "calcel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -49,5 +101,36 @@ class ViewController: UIViewController, WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress"{
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host{
+            for website in websites{
+                if host.contains(website){
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel)
+        let ac = UIAlertController(title: "Warning", message: "Danger", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OMG", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+    
+    
+    
 }
-
+/*
+ extension ViewController : UITableViewDataSource, UITableViewDelegate{
+ 
+ }
+ */
