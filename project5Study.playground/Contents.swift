61e90e276f917320be08da74a1f3dd1b36577108
JaeWook Lee <2jae66@gmail.com>
2020-05-09 18:14:22 +0900

    project5

---
+++ project5Study.playground/Contents.swift
@@ -0,0 +1,20 @@
+class Singer{
+    func playSong(){
+        print("Shake it off!")
+    }
+}
+
+func sing() -> () -> Void{
+    let taylor = Singer()
+    let singing = {[weak taylor] in
+        taylor?.playSong()
+        return
+    }
+    
+    return singing
+}
+
+
+let singFunction = sing()
+singFunction()
+print("gg")
