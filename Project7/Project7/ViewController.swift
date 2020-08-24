76a02943f533c345771d98c23f5e64f2788df27e
JaeWook Lee <2jae66@gmail.com>
2020-08-24 20:18:31 +0900

    project7

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by 1 on 2020/07/16.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
