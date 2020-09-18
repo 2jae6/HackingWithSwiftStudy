6d268607d2c9ac776b403c235a98916778c2e15a
JaeWook Lee <2jae66@gmail.com>
2020-09-18 21:10:54 +0900

    33day

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,17 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by 1 on 2020/09/18.
+//
+
+import Foundation
+
+
+struct Petition: Codable{
+    
+    var title: String
+    var body: String
+    var signatureCount: Int
+    
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by 1 on 2020/09/18.
+//
+
+import Foundation
+
+struct Petitions:Codable {
+    var results: [Petition]
+
+}
